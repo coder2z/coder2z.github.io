ab6ae5c3884ebad23c5fcf4c77f27c17e9b093df
myxy99 <2399158611@qq.com>
2020-07-21 14:34:26 +0800

    Site updated: 2020-07-21 14:34:10

--- js/queue.js
+++ js/queue.js
@@ -1,3 +1,3 @@
-// build time:Tue Jul 21 2020 14:29:31 GMT+0800 (GMT+08:00)
+// build time:Tue Jul 21 2020 14:34:08 GMT+0800 (GMT+08:00)
 function Queue(){this.dataStore=[];this.offer=e;this.poll=t;this.execNext=u;this.debug=false;this.startDebug=o;function e(e){if(this.debug){console.log("Offered a Queued Function.")}if(typeof e==="function"){this.dataStore.push(e)}else{console.log("You must offer a function.")}}function t(){if(this.debug){console.log("Polled a Queued Function.")}return this.dataStore.shift()}function u(){var e=this.poll();if(e!==undefined){if(this.debug){console.log("Run a Queued Function.")}e()}}function o(){this.debug=true}}var queue=new Queue;
 //rebuild by neat 
